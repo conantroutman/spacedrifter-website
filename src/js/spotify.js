0c370d096d7bc09a9e08212bf60f6c39bdd652aa
conantroutman <johsod96@gmail.com>
2020-12-04 16:55:31 +0100

    Fetching Spotify data from API instead

--- src/js/spotify.js
+++ src/js/spotify.js
@@ -1,21 +1,20 @@
-const releaseData = require('../releases.json');
+const url = 'https://spacedrifter-backend.herokuapp.com/api/v1/spotify';
 
-const getSpotifyReleases = () => {
+const getSpotifyReleases = async () => {
     const element = document.querySelector(".releases");
-    fetch(releaseData)
+    await fetch(url)
         .then(response => response.json())
         .then(data => {
-            data.releases.forEach(release => {
-                console.log(release.title);
+            data.items.forEach(item => {
                 const html = `
                 <div class="release">
                     <div class="cover-image">
-                        <img src="${release.cover}"></img>
+                        <img src="${item.images[0].url}"></img>
                     </div>
-                    <iframe src="https://open.spotify.com/embed/album/${release.spotifyId}" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-                    <h3>${release.title}</h3>
-                    <div>${new Date(release.date).getFullYear()}</div>
-                    <div>${release.type}</div>
+                    <iframe src="https://open.spotify.com/embed/album/${item.id}" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                    <h3 class="album-title">${item.name}</h3>
+                    <div class="album-releasedate">${new Date(item.release_date).getFullYear()}</div>
+                    <div class="album-type">${item.album_type}</div>
                 </div>
                 `;
                 element.innerHTML += html;
