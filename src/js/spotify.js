c2a192dd9944437b2940ba57a47ea6bfee51fbe2
conantroutman <johsod96@gmail.com>
2021-03-30 10:40:12 +0200

    Fixed bug where EPs are displayed as singles

--- src/js/spotify.js
+++ src/js/spotify.js
@@ -12,10 +12,10 @@ export const getSpotifyReleases = async () => {
                     <div class="cover-image">
                         <a href="${item.external_urls.spotify}" target="_blank" title="${item.name} on Spotify" ><img src="${item.images[0].url}"></img></a>
                     </div>
-                    <iframe src="https://open.spotify.com/embed/album/${item.id}" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                    <iframe src="https://open.spotify.com/embed/album/${item.id}" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media" loading="lazy"></iframe>
                     <h3 class="album-title">${item.name}</h3>
                     <div class="album-releasedate">${new Date(item.release_date).getFullYear()}</div>
-                    <div class="album-type">${item.album_type}</div>
+                    <div class="album-type">${getReleaseType(item.album_type, item.total_tracks)}</div>
                 </div>
                 `;
                 element.innerHTML += html;
@@ -25,4 +25,18 @@ export const getSpotifyReleases = async () => {
         .catch((error) => {
             document.querySelector(".music-section").style.display = "none";
         })
+}
+
+// Returns Single, EP or Album depending on type and total number of tracks. This function is needed because the Spotify API does not distinguish between EPs and singles
+function getReleaseType(type, tracks){
+    let returnString = '';
+
+    if (type == 'single') {
+        returnString = tracks > 1 ? 'EP' : 'Single';
+    }
+    else if (type == 'album') {
+        returnString = 'Album';
+    }
+
+    return returnString;
 }
