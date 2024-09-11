349e3034038cd97d01b2228e093c31a78f7338c0
John Söderberg <john.co.soderberg@gmail.com>
2024-09-11 23:00:30 +0200

    Added Github Pages 404 page

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,14 @@
+<!-- Custom 404 page for GitHub Pages -->
+
+<script lang="ts">
+	import Container from '$lib/Container.svelte';
+	import Heading from '$lib/Heading.svelte';
+	import RandomYoutubeVideo from '$lib/RandomYoutubeVideo.svelte';
+</script>
+
+<Container>
+	<Heading>404 Not Found</Heading>
+	<p>Sorry about that.</p>
+	<p>Here's a random Youtube video!</p>
+	<RandomYoutubeVideo />
+</Container>
