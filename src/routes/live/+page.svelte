f6d89c6a5c7dab68c42f2f0ae6795a9ecccce31b
John Söderberg <john.co.soderberg@gmail.com>
2024-09-11 20:12:45 +0200

    Updated bullshit text

--- src/routes/live/+page.svelte
+++ src/routes/live/+page.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
-	import Container from '$lib/Container.svelte';
-	import Heading from '$lib/Heading.svelte';
-	import Section from '$lib/Section.svelte';
 	import { gigs } from './data';
-	import Gig from './Gig.svelte';
 	import GigList from './GigList.svelte';
 
 	const currentDate = new Date();
@@ -13,4 +9,4 @@
 </script>
 
 <GigList data={futureGigs} heading="Future Gigs" bullshitText="Look towards the future!" />
-<GigList data={pastGigs} heading="Past Gigs" bullshitText="You can't change the past man..." />
+<GigList data={pastGigs} heading="Past Gigs" bullshitText="Blast from the past!" />
