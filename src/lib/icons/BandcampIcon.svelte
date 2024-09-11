d5903350f8b8d50ae2d2444634141d007cda564c
John Söderberg <john.co.soderberg@gmail.com>
2024-09-11 18:16:02 +0200

    Improved icons

--- src/lib/icons/BandcampIcon.svelte
+++ src/lib/icons/BandcampIcon.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-	export let color: string = '#fff';
-</script>
-
-<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill={color}
+<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
 	><path d="M0 18.75l7.437-13.5H24l-7.438 13.5H0z"></path></svg
 >
