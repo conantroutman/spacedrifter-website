260d3722b29267aef38170f54b2dcd2abd21163b
John Söderberg <john.co.soderberg@gmail.com>
2024-11-30 03:24:39 +0100

    Fix swirl background

--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -3,10 +3,10 @@
 	export let backgroundColor: string = 'inherit';
 </script>
 
-<section style:--section-background-color={backgroundColor}>
-	{#if swirlBackground}
-		<div class:swirl-background={swirlBackground} class="background" />
-	{/if}
+<section
+	style:--section-background-color={backgroundColor}
+	class:swirl-background={swirlBackground}
+>
 	<div class="content">
 		<slot />
 	</div>
@@ -21,17 +21,20 @@
 		position: relative;
 	}
 
-	.background {
+	section.swirl-background::after {
+		content: '';
 		position: absolute;
 		inset: 0;
-		opacity: 0.5;
-	}
-
-	.swirl-background {
+		opacity: 0.25;
 		background-image: url('$lib/assets/swirls.svg');
 		background-position: center;
 		background-repeat: no-repeat;
 		background-size: max(100vw, 2000px);
 		background-blend-mode: multiply;
+	}
+
+	.content {
+		position: relative;
+		z-index: 2;
 	}
 </style>
