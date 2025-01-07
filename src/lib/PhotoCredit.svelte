39d48ab41443742b2ae770596a0d46e60c538926
John Söderberg <johsod96@gmail.com>
2025-01-07 17:24:21 +0100

    Update photo credit styling

--- src/lib/PhotoCredit.svelte
+++ src/lib/PhotoCredit.svelte
@@ -19,8 +19,9 @@
 		left: 0;
 		bottom: 0;
 		background-color: rgba(0, 0, 0, 0.65);
-		color: #fff;
+		color: rgba(255, 255, 255, 0.8);
 		padding: 0.25rem;
 		z-index: 10;
+		font-size: 0.75rem;
 	}
 </style>
